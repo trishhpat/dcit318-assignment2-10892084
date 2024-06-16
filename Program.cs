6825138ab74239c49b7e1b5d37dbf4c5ecc9b13c
trishhpat <pamensah036@st.ug.edu.gh>
2024-06-16 11:31:59 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace MyProject
+{
+    public interface IMovable
+    {
+        public void Move();
+    }
+
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Move();
+
+            Bicycle bicycle = new Bicycle();
+            bicycle.Move();
+        }
+    }
+}
